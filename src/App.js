6ec7e3086167e5dcc9eb70e5a0e03b6cc38a3345
Daniel Nam <dannam83@gmail.com>
2018-10-26 08:27:18 -0400

    add login button

--- src/App.js
+++ src/App.js
@@ -1,13 +1,27 @@
 import React, { Component } from 'react';
 import { Text, View } from 'react-native';
+import firebase from 'firebase';
 import { Header } from './components/common';
+import LoginForm from './components/LoginForm';
 
 class App extends Component {
+  componentWillMount() {
+    const config = {
+      apiKey: 'AIzaSyAnyqYFEmON9P8GqnnRYLvn66KoFjtcRG8',
+      authDomain: 'auth-13ae9.firebaseapp.com',
+      databaseURL: 'https://auth-13ae9.firebaseio.com',
+      projectId: 'auth-13ae9',
+      storageBucket: 'auth-13ae9.appspot.com',
+      messagingSenderId: '843003811955'
+    };
+    firebase.initializeApp(config);
+  }
+
   render() {
     return (
         <View>
           <Header headerText="Authorization" />
-          <Text>Hello World</Text>
+          <LoginForm />
         </View>
     );
   }
