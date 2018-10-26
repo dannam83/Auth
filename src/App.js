376dabd77fc73a5f88fb69c4e0b9440b5d02b4ea
Daniel Nam <dannam83@gmail.com>
2018-10-25 20:24:01 -0400

    fix to working version of react

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import React, { Component } from 'react';
 import { Text, View } from 'react-native';
+import { Header } from './components/common';
 
 class App extends Component {
   render() {
     return (
-      <View>
-        <Text>Hello World</Text>
-      </View>
+        <View>
+          <Header headerText="Authorization" />
+          <Text>Hello World</Text>
+        </View>
     );
   }
 }
